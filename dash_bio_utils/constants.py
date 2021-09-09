349be6b2e6fc17ea746631de8f3d72e7121c68da
Hammad Khan <hammadtheone@gmail.com>
2021-09-09 19:31:02 -0400

    Linting and gitignore updates

--- dash_bio_utils/constants.py
+++ dash_bio_utils/constants.py
@@ -7,7 +7,7 @@ ATOM_COLOR_DICT = {
     "F": "#ffff00",
     "P": "#ffa500",
     "K": "#42f4ee",
-    "G": "#3f3f3f"
+    "G": "#3f3f3f",
 }
 
 CHAIN_COLOR_DICT = {
@@ -28,52 +28,52 @@ CHAIN_COLOR_DICT = {
     "O": "#008080",
     "P": "#008080",
     "x": "#9c6677",
-    "Y": "#b7c5c8"
+    "Y": "#b7c5c8",
 }
 
 RESIDUE_COLOR_DICT = {
-    'ALA': '#C8C8C8',
-    'ARG': '#145AFF',
-    'ASN': '#00DCDC',
-    'ASP': '#E60A0A',
-    'CYS': '#E6E600',
-    'GLN': '#00DCDC',
-    'GLU': '#E60A0A',
-    'GLY': '#EBEBEB',
-    'HIS': '#8282D2',
-    'ILE': '#0F820F',
-    'LEU': '#0F820F',
-    'LYS': '#145AFF',
-    'MET': '#E6E600',
-    'PHE': '#3232AA',
-    'PRO': '#DC9682',
-    'SER': '#FA9600',
-    'THR': '#FA9600',
-    'TRP': '#B45AB4',
-    'TYR': '#3232AA',
-    'VAL': '#0F820F',
-    'ASX': '#FF69B4',
-    'GLX': '#FF69B4',
-    'A': '#A0A0FF',
-    'DA': '#A0A0FF',
-    'G': '#FF7070',
-    'DG': '#FF7070',
-    'I': '#80FFFF',
-    'C': '#FF8C4B',
-    'DC': '#FF8C4B',
-    'T': '#A0FFA0',
-    'DT': '#A0FFA0',
-    'U': '#FF8080'
+    "ALA": "#C8C8C8",
+    "ARG": "#145AFF",
+    "ASN": "#00DCDC",
+    "ASP": "#E60A0A",
+    "CYS": "#E6E600",
+    "GLN": "#00DCDC",
+    "GLU": "#E60A0A",
+    "GLY": "#EBEBEB",
+    "HIS": "#8282D2",
+    "ILE": "#0F820F",
+    "LEU": "#0F820F",
+    "LYS": "#145AFF",
+    "MET": "#E6E600",
+    "PHE": "#3232AA",
+    "PRO": "#DC9682",
+    "SER": "#FA9600",
+    "THR": "#FA9600",
+    "TRP": "#B45AB4",
+    "TYR": "#3232AA",
+    "VAL": "#0F820F",
+    "ASX": "#FF69B4",
+    "GLX": "#FF69B4",
+    "A": "#A0A0FF",
+    "DA": "#A0A0FF",
+    "G": "#FF7070",
+    "DG": "#FF7070",
+    "I": "#80FFFF",
+    "C": "#FF8C4B",
+    "DC": "#FF8C4B",
+    "T": "#A0FFA0",
+    "DT": "#A0FFA0",
+    "U": "#FF8080",
 }
 
 AMINO_ACID_CLASSES = {  # pylint: disable=invalid-name
-    'hydrophobic': ['GLY', 'ALA', 'LEU', 'ILE', 'VAL', 'MET', 'PRO'],
-    'polar': ['ASN', 'GLN', 'SER', 'THR', 'CYS'],
-    'acidic': ['ASP', 'GLU'],
-    'basic': ['LYS', 'ARG', 'HIS'],
-    'aromatic': ['TRP', 'TYR', 'PHE'],
-    'purine': ['A', 'G', 'DA', 'DG'],
-    'pyrimidine': ['DT', 'DC', 'U', 'I', 'C']
+    "hydrophobic": ["GLY", "ALA", "LEU", "ILE", "VAL", "MET", "PRO"],
+    "polar": ["ASN", "GLN", "SER", "THR", "CYS"],
+    "acidic": ["ASP", "GLU"],
+    "basic": ["LYS", "ARG", "HIS"],
+    "aromatic": ["TRP", "TYR", "PHE"],
+    "purine": ["A", "G", "DA", "DG"],
+    "pyrimidine": ["DT", "DC", "U", "I", "C"],
 }
 
 RESIDUE_TYPES = {}
@@ -82,11 +82,11 @@ for aa_type in AMINO_ACID_CLASSES:
         RESIDUE_TYPES[aa] = aa_type
 
 RESIDUE_TYPE_COLOR_DICT = {
-    'hydrophobic': '#00ff80',
-    'polar': '#ff00bf',
-    'acidic': '#ff4000',
-    'basic': '#0040ff',
-    'aromatic': '#ffff00',
-    'purine': '#A00042',
-    'pyrimidine': '#4F4600'
+    "hydrophobic": "#00ff80",
+    "polar": "#ff00bf",
+    "acidic": "#ff4000",
+    "basic": "#0040ff",
+    "aromatic": "#ffff00",
+    "purine": "#A00042",
+    "pyrimidine": "#4F4600",
 }
--- dash_bio_utils/pdb_parser.py
+++ dash_bio_utils/pdb_parser.py
@@ -5,9 +5,15 @@ JSON representation of the structural data and styling."""
 
 import re
 import six.moves.urllib.request as urlreq
-from  six.moves.urllib.parse import urlparse as urlparse
+from six.moves.urllib.parse import urlparse as urlparse
 from six.moves.urllib.error import URLError
-from .constants import ATOM_COLOR_DICT, CHAIN_COLOR_DICT, RESIDUE_COLOR_DICT, RESIDUE_TYPES, RESIDUE_TYPE_COLOR_DICT
+from .constants import (
+    ATOM_COLOR_DICT,
+    CHAIN_COLOR_DICT,
+    RESIDUE_COLOR_DICT,
+    RESIDUE_TYPES,
+    RESIDUE_TYPE_COLOR_DICT,
+)
 
 import parmed as pmd
 
@@ -15,6 +21,7 @@ import parmed as pmd
 The following functions specify the style of a
 molecule that is rendered using the Molecule3dViewer component in Dash
 Bio."""
+
 
 def fill_in_defaults(input_dict, default_dict):
     """Function to automatically populate any missing values in the
@@ -29,13 +36,13 @@ def fill_in_defaults(input_dict, default_dict):
 
 
 def create_style(
-        lines,
-        style,
-        mol_color,
-        residue_type_colors=None,
-        atom_colors=None,
-        chain_colors=None,
-        residue_colors=None
+    lines,
+    style,
+    mol_color,
+    residue_type_colors=None,
+    atom_colors=None,
+    chain_colors=None,
+    residue_colors=None,
 ):
     """Function to create the different styles (stick, cartoon, sphere)
     using the protein data bank (PDB) file as input. This function outputs
@@ -55,14 +62,10 @@ def create_style(
     """
 
     # Merge dictionaries if necessary
-    residue_type_colors = fill_in_defaults(residue_type_colors,
-                                           RESIDUE_TYPE_COLOR_DICT)
-    atom_colors = fill_in_defaults(atom_colors,
-                                   ATOM_COLOR_DICT)
-    chain_colors = fill_in_defaults(chain_colors,
-                                    CHAIN_COLOR_DICT)
-    residue_colors = fill_in_defaults(residue_colors,
-                                      RESIDUE_COLOR_DICT)
+    residue_type_colors = fill_in_defaults(residue_type_colors, RESIDUE_TYPE_COLOR_DICT)
+    atom_colors = fill_in_defaults(atom_colors, ATOM_COLOR_DICT)
+    chain_colors = fill_in_defaults(chain_colors, CHAIN_COLOR_DICT)
+    residue_colors = fill_in_defaults(residue_colors, RESIDUE_COLOR_DICT)
 
     # Initialize variables
     chains = []
@@ -73,11 +76,7 @@ def create_style(
 
     # Variables that store the character positions of different
     # parameters from the molecule PDB file
-    pos = {
-        'chain': [21, 22],
-        'atm_type': [77, 78],
-        'res_name': [17, 20]
-    }
+    pos = {"chain": [21, 22], "atm_type": [77, 78], "res_name": [17, 20]}
 
     for l in lines:
         line = l.split()
@@ -86,15 +85,9 @@ def create_style(
         if "ATOM" not in line[0] and "HETATM" not in line[0]:
             continue
 
-        chain = l[
-            pos['chain'][0]:pos['chain'][1]
-        ]
-        atm_type = l[
-            pos['atm_type'][0]:pos['atm_type'][1]
-        ]
-        res_name = l[
-            pos['res_name'][0]:pos['res_name'][1]
-        ].strip()
+        chain = l[pos["chain"][0] : pos["chain"][1]]
+        atm_type = l[pos["atm_type"][0] : pos["atm_type"][1]]
+        res_name = l[pos["res_name"][0] : pos["res_name"][1]].strip()
 
         chains.append(chain)
         atm_types.append(atm_type)
@@ -103,51 +96,48 @@ def create_style(
         index = len(chains) - 1
 
         if line[0] == "ATOM":
-            if mol_color == 'chain':
+            if mol_color == "chain":
                 style_data[index] = {
-                    'color': chain_colors[
-                        chain
-                    ] if chain in chain_colors else '#BEA06E',
-                    'visualization_type': style
+                    "color": chain_colors[chain]
+                    if chain in chain_colors
+                    else "#BEA06E",
+                    "visualization_type": style,
                 }
-            elif mol_color == 'residue':
+            elif mol_color == "residue":
                 style_data[index] = {
-                    'color': residue_colors[
-                        res_name.upper()
-                    ] if res_name.upper() in residue_colors else '#BEA06E',
-                    'visualization_type': style
+                    "color": residue_colors[res_name.upper()]
+                    if res_name.upper() in residue_colors
+                    else "#BEA06E",
+                    "visualization_type": style,
                 }
-            elif mol_color == 'residue_type':
+            elif mol_color == "residue_type":
                 style_data[index] = {
-                    'color': residue_type_colors[
-                        RESIDUE_TYPES[res_name.upper()]
-                    ] if res_name.upper() in RESIDUE_TYPES else '#BEA06E',
-                    'visualization_type': style
+                    "color": residue_type_colors[RESIDUE_TYPES[res_name.upper()]]
+                    if res_name.upper() in RESIDUE_TYPES
+                    else "#BEA06E",
+                    "visualization_type": style,
                 }
-            elif mol_color == 'atom':
+            elif mol_color == "atom":
                 style_data[index] = {
-                    'color': atom_colors[
-                        atm_type
-                    ] if atm_type in atom_colors else '#330000',
-                    'visualization_type': style
+                    "color": atom_colors[atm_type]
+                    if atm_type in atom_colors
+                    else "#330000",
+                    "visualization_type": style,
                 }
 
         else:
             if atm_type in atom_colors:
                 style_data[index] = {
                     "color": atom_colors[atm_type],
-                    "visualization_type": "stick"
+                    "visualization_type": "stick",
                 }
             else:
-                style_data[index] = {
-                    "color": "#330000",
-                    "visualization_type": "stick"
-                }
+                style_data[index] = {"color": "#330000", "visualization_type": "stick"}
 
     return style_data
 
 
-def create_data(pdb_path, style='cartoon', mol_color='residue', **kwargs):
+def create_data(pdb_path, style="cartoon", mol_color="residue", **kwargs):
     """
     Parse the protein data bank (PDB) file to generate
     input modelData for the Molecule3dViewer component
@@ -172,19 +162,20 @@ def create_data(pdb_path, style='cartoon', mol_color='residue', **kwargs):
     top = pmd.load_file(pdb_path)
 
     # Check if the pdb_path points to a URL and decode it if necessary
-    if (urlparse(pdb_path).scheme != ''):
+    if urlparse(pdb_path).scheme != "":
         try:
-            file =  urlreq.urlopen(pdb_path)
-            lines = [l.decode('utf-8').strip() for l in file if l.decode('UTF-8').strip()]
+            file = urlreq.urlopen(pdb_path)
+            lines = [
+                l.decode("utf-8").strip() for l in file if l.decode("UTF-8").strip()
+            ]
         except URLError as e:
             print(e.reason)
 
     else:
         # Read PDB file to create atom/bond information
-        with open(pdb_path, 'r') as infile:
+        with open(pdb_path, "r") as infile:
             # store only non-empty lines
             lines = [l.strip() for l in infile if l.strip()]
-
 
     # Initialize all variables
     var_nchains = []
@@ -199,10 +190,7 @@ def create_data(pdb_path, style='cartoon', mol_color='residue', **kwargs):
     atom_type = []
     ct = 0
 
-    modelData = {
-        'atoms': [],
-        'bonds': []
-    }
+    modelData = {"atoms": [], "bonds": []}
 
     # Variables that store the character positions of different
     # parameters from the molecule PDB file
@@ -221,23 +209,23 @@ def create_data(pdb_path, style='cartoon', mol_color='residue', **kwargs):
     for l in lines:
         line = l.split()
         if "ATOM" in line[0] or "HETATM" in line[0]:
-            serial.append(int(l[serialpos[0]:serialpos[1]]))
-            atm_name.append(l[atm_namepos[0]:atm_namepos[1]].strip())
-            val_r_name = l[r_namepos[0]:r_namepos[1]].strip()
+            serial.append(int(l[serialpos[0] : serialpos[1]]))
+            atm_name.append(l[atm_namepos[0] : atm_namepos[1]].strip())
+            val_r_name = l[r_namepos[0] : r_namepos[1]].strip()
             res_name.append(val_r_name)
-            chain_val = l[chainpos[0]:chainpos[1]].strip()
+            chain_val = l[chainpos[0] : chainpos[1]].strip()
             chain.append(chain_val)
             if chain_val not in var_nchains:
                 var_nchains.append(chain_val)
-            val_r_id = int(l[r_idpos[0]:r_idpos[1]])
+            val_r_id = int(l[r_idpos[0] : r_idpos[1]])
             res_id.append(val_r_id)
-            x = float(l[xpos[0]:xpos[1]])
-            y = float(l[ypos[0]:ypos[1]])
-            z = float(l[zpos[0]:zpos[1]])
+            x = float(l[xpos[0] : xpos[1]])
+            y = float(l[ypos[0] : ypos[1]])
+            z = float(l[zpos[0] : zpos[1]])
             positions.append([x, y, z])
-            occupancy.append(l[occupos[0]:occupos[1]].strip())
-            temp_factor.append(l[bfacpos[0]:bfacpos[1]].strip())
-            atom_type.append(l[atm_typepos[0]:atm_typepos[1]].strip())
+            occupancy.append(l[occupos[0] : occupos[1]].strip())
+            temp_factor.append(l[bfacpos[0] : bfacpos[1]].strip())
+            atom_type.append(l[atm_typepos[0] : atm_typepos[1]].strip())
             ct += 1
 
     # Create list of atoms
@@ -247,27 +235,27 @@ def create_data(pdb_path, style='cartoon', mol_color='residue', **kwargs):
         if tmp_res != res_id[i]:
             tmp_res = res_id[i]
             resct += 1
-        modelData['atoms'].append({
-            "name": atm_name[i],
-            "chain": chain[i],
-            "positions": positions[i],
-            "residue_index": resct,
-            "element": atom_type[i],
-            "residue_name": res_name[i] + str(res_id[i]),
-            "serial": i,
-        })
+        modelData["atoms"].append(
+            {
+                "name": atm_name[i],
+                "chain": chain[i],
+                "positions": positions[i],
+                "residue_index": resct,
+                "element": atom_type[i],
+                "residue_name": res_name[i] + str(res_id[i]),
+                "serial": i,
+            }
+        )
 
     # Create list of bonds using the parmed module
     for i in range(len(top.bonds)):
         bondpair = top.bonds[i].__dict__
-        atom1 = re.findall(r"\[(\d+)\]", str(bondpair['atom1']))
-        atom2 = re.findall(r"\[(\d+)\]", str(bondpair['atom2']))
-        modelData['bonds'].append({
-            'atom2_index': int(atom1[0]),
-            'atom1_index': int(atom2[0])
-        })
+        atom1 = re.findall(r"\[(\d+)\]", str(bondpair["atom1"]))
+        atom2 = re.findall(r"\[(\d+)\]", str(bondpair["atom2"]))
+        modelData["bonds"].append(
+            {"atom2_index": int(atom1[0]), "atom1_index": int(atom2[0])}
+        )
 
     styles_data = create_style(lines, style=style, mol_color=mol_color, **kwargs)
 
-
-    return {'styles': styles_data, 'modelData': modelData}
+    return {"styles": styles_data, "modelData": modelData}
